3c01c4007960db57f998214645b1860141bc6247
0xSavant <100378695+0xSavant@users.noreply.github.com>
2023-02-28 09:38:48 -0500

    add disclaimer

--- programs/Stockpile/src/lib.rs
+++ programs/Stockpile/src/lib.rs
@@ -18,8 +18,17 @@
 █████╗█████╗█████╗
 ╚════╝╚════╝╚════╝
 
+Copyright 2022 Stockpile,
+www.twitter.com/GoStockpile
+www.stockpile.pro
 
+--GPL 3.0 License--
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+DISCLAIMER:
+This code is currently unaudited, while reusing and duplication are allowed, please do so at your own risk.
 */
 
 use anchor_lang::prelude::*;
